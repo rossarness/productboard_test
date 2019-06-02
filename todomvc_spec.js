2425883a01ac1d7b4142f638f080d984e32745a8
Jakub Gabryel <rossarness@gmail.com>
2019-06-02 17:50:02 +0200

    Improved test organisation

--- todomvc_spec.js
+++ todomvc_spec.js
@@ -1,178 +1,191 @@
 /// <reference types="Cypress" />
 
-var text = "Lorem ipsum dolor sit amet"
-var long_text =  "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?"
+const text = "Lorem ipsum dolor sit amet"
+const long_text = "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?"
+const exoticItems = ["ฤดูหนาวกำลังจะมา", "겨울이오고있다.", "ਵਿੰਟਰ ਆ ਰਿਹਾ ਹੈ"]
+const pageTitle = "todos"
+const pageFooter = "[data-cy=footer]"
+const input = "[data-cy=new-todo-input]"
+const allFilter = ":nth-child(1) > a"
+const activeFilter = ":nth-child(2) > a"
+const completedFilter = ":nth-child(3) > a"
+const itemsForFilteringTest = 15
+const todoItem = "[data-cy='todo-item']"
+const longItemsAmount = 2
+const toogleButton = "> .view > [data-cy=toggle]"
+const removeItemButton = "> .view > [data-cy='todo-item-remove']"
+const filtersBox = "[data-cy=filters]"
+const filtersCount = 3
 var active = 0
 
 context('TodoMVC', () => {
   before(() => {
     cy.visit('http://localhost:3000/');
   });
+
   it('Verify website elements', () => {
-    cy.contains("todos")
-    cy.get("[data-cy=new-todo-input]")
+    cy.contains(pageTitle)
+    cy.get(input)
     .should("be.visible")
   })
+  
   it('Add one item', () => {
-    cy.get("[data-cy=new-todo-input]")
+    cy.get(input)
     .should("be.empty")
     .type(text)
     .should("have.value", text)
     .type("{enter}")
     .should("have.text", "")
   })
+
   it('Verify added item', () => {
-    cy.get('[data-cy=todo-item-label]')
+    cy.get(todoItem)
     .contains(text)
     .should("be.visible")
-    cy.get(':nth-child(1) > .view > [data-cy=toggle]')
+    cy.get(':nth-child(1) ' + toogleButton)
     .should("not.be.selected")
-    cy.get(':nth-child(1) > .view > [data-cy="todo-item-remove"]')
+    cy.get(':nth-child(1) ' + removeItemButton)
     .should("not.be.visible")
     .invoke("show")
     .should("be.visible")
     .invoke("hide")
   })
+
   it('Verify footer menu', () => {
-    cy.get('[data-cy=footer]')
+    cy.get(pageFooter)
     .should("be.visible")
     .contains("1 item left")
-    cy.get("[data-cy=filters]")
+    cy.get(filtersBox)
     .should("be.visible")
-    .children().should("have.length", 3)
-    cy.get(':nth-child(1) > a')
+    .children().should("have.length", filtersCount)
+    cy.get(allFilter)
     .should("have.class", "selected")
     .and("have.html", "All")
   })
+
   it('Delete one item', () => {
-    cy.get(':nth-child(1) > .view > [data-cy="todo-item-remove"]')
+    cy.get(':nth-child(1) ' + removeItemButton)
     .invoke("show")
     .click()
-    cy.get('[data-cy=footer]')
+    cy.get(pageFooter)
     .should("not.be.visible")
-    cy.get('[data-cy=todo-item-label]')
+    cy.get(todoItem)
     .should("not.be.visible")
   })
+
   it('Test filtering', () => {
     var completed = new Array;
     var active_label = new Array;
-    for (var i = 1; i <= 15; i++) { 
-      var item_text = i + " " + text
-      cy.get("[data-cy=new-todo-input]")
+    for (var i = 1; i <= itemsForFilteringTest; i++) { 
+      var item_text = i + text
+      cy.get(input)
       .type(item_text)
       .type("{enter}")
       if (i % 2 === 0) {
         completed.push(item_text)
-        cy.get(':nth-child('+ i +') > .view > [data-cy=toggle]')
+        cy.get(':nth-child('+ i +') ' + toogleButton)
         .click()
-        cy.get(':nth-child('+ i +')[data-cy="todo-item"]')
+        cy.get(':nth-child('+ i +')' + todoItem)
         .should("have.class", "completed")
       } else {
         active_label.push(item_text)
       }
     }
-    active = (15 - completed.length)
-    cy.get(':nth-child(2) > a')
+    active = (itemsForFilteringTest - completed.length)
+    cy.get(activeFilter)
     .should("have.html", "Active")
     .should("not.have.class", "selected")
     .click()
     .should("have.class", "selected")
-    cy.get(':nth-child(1) > a')
+    cy.get(allFilter)
     .should("not.have.class", "selected")
-    cy.get(':nth-child(3) > a')
+    cy.get(completedFilter)
     .should("not.have.class", "selected")
-    cy.get('[data-cy=footer]')
+    cy.get(pageFooter)
     .should("be.visible")
     .contains(active + " items left")
     for (var i = 1; i <= active; i++){
-      cy.get(':nth-child('+ i +')[data-cy="todo-item"]')
+      cy.get(':nth-child('+ i + ')' + todoItem)
       .should("have.text", active_label[i - 1])
       .should("not.have.class", "completed")      
     }
-    cy.get(':nth-child(3) > a')
+    cy.get(completedFilter)
     .should("have.html", "Completed")
     .should("not.have.class", "selected")
     .click()
     .should("have.class", "selected")
-    cy.get(':nth-child(1) > a')
+    cy.get(allFilter)
     .should("not.have.class", "selected")
-    cy.get(':nth-child(2) > a')
+    cy.get(activeFilter)
     .should("not.have.class", "selected")
-    cy.get('[data-cy=footer]')
+    cy.get(pageFooter)
     .should("be.visible")
     .contains(active + " items left")
     for (var i = 1; i <= completed.length; i++){
-      cy.get(':nth-child('+ i +')[data-cy="todo-item"]')
+      cy.get(':nth-child('+ i +')' + todoItem)
       .should("have.text", completed[i - 1])
       .should("have.class", "completed")      
     }
-    cy.get(':nth-child(1) > a')
+    cy.get(allFilter)
     .should("have.html", "All")
     .should("not.have.class", "selected")
     .click()
     .should("have.class", "selected")
-    cy.get(':nth-child(2) > a')
+    cy.get(activeFilter)
     .should("not.have.class", "selected")
-    cy.get(':nth-child(3) > a')
+    cy.get(completedFilter)
     .should("not.have.class", "selected")
-    cy.get(':nth-child(15)[data-cy="todo-item"]')
-    .should("have.text", "15 " + text)
+    cy.get(':nth-child(' + itemsForFilteringTest + ')' + todoItem)
+    .should("have.text", itemsForFilteringTest + text)
     .should("not.have.class", "completed")  
   })
+
   it('Clear completed items', () => {
-    cy.get(':nth-child(3) > a')
+    cy.get(completedFilter)
     .click()
     cy.get('.clear-completed')
     .click()
-    cy.get('[data-cy=footer]')
+    cy.get(pageFooter)
     .should("be.visible")
     .contains(active + " items left")
-    cy.get('[data-cy=todo-item-label]')
+    cy.get(todoItem)
     .should("not.be.visible")
-    cy.get(':nth-child(1) > a')
+    cy.get(allFilter)
     .click()
-    cy.get(':nth-child(2)[data-cy="todo-item"]')
-    .should("have.text", "3 " + text)
+    cy.get(':nth-child(2)' + todoItem)
+    .should("have.text", "3" + text)
   })
+
   it('Add very long items', () => {
-    cy.get("[data-cy=new-todo-input]")
-    .type(long_text)
-    .should("have.value", long_text)
-    .type("{enter}")
-    active++
-    cy.get("[data-cy=new-todo-input]")
-    .type(long_text)
-    .should("have.value", long_text)
-    .type("{enter}")
-    active++
+    for (var i = 1; i <= longItemsAmount; i++){
+      cy.get(input)
+      .type(long_text)
+      .should("have.value", long_text)
+      .type("{enter}")
+      active++
+    }
   })
+
   it('Add items with exotic chars', () => {
-    cy.get("[data-cy=new-todo-input]")
-    .type("ฤดูหนาวกำลังจะมา")
-    .should("have.value", "ฤดูหนาวกำลังจะมา")
-    .type("{enter}")
-    active++
-    cy.get("[data-cy=new-todo-input]")
-    .type("겨울이오고있다.")
-    .should("have.value", "겨울이오고있다.")
-    .type("{enter}")
-    active++
-    cy.get("[data-cy=new-todo-input]")
-    .type("ਵਿੰਟਰ ਆ ਰਿਹਾ ਹੈ")
-    .should("have.value", "ਵਿੰਟਰ ਆ ਰਿਹਾ ਹੈ")
-    .type("{enter}")
-    active++
-    cy.get('[data-cy=footer]')
+    for (var i = 0; i < exoticItems.length; i++){
+      cy.get(input)
+      .type(exoticItems[i])
+      .should("have.value", exoticItems[i])
+      .type("{enter}")
+      active++
+    }
+    cy.get(pageFooter)
     .should("be.visible")
     .contains(active + " items left")
   })
+
   it("Add hundred items",() => {
     for (var i = 0; i < 100; i++) { 
-      cy.get("[data-cy=new-todo-input]")
+      cy.get(input)
       .type(text)
       .type("{enter}")
       active++
-      cy.get('[data-cy=footer]')
+      cy.get(pageFooter)
       .should("be.visible")
       .contains(active + " items left")
     }
@@ -184,7 +197,7 @@ context('TodoMVC', () => {
       .click()
       active-- 
     }
-    cy.get('[data-cy=footer]')
+    cy.get(pageFooter)
     .should("be.visible")
     .contains(active + " items left")
   })
